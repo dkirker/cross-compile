584554c5eee8c8b1827cf17e5c9635926d39586a
Malte Schröder <maltesch@gmx.de>
2010-03-23 18:32:34 +0100

    Fix rejects

--- nonworking/navit/patches/navit__gui__internal__gui_internal.c
+++ nonworking/navit/patches/navit__gui__internal__gui_internal.c
@@ -1,21 +1,13 @@
-*** build/src/navit/gui/internal/gui_internal.c	2010-03-17 22:51:42.000000000 +0100
---- gui_internal.c	2010-03-17 22:51:05.000000000 +0100
-***************
-*** 5901,5908 ****
-  	      this->text_foreground_color=*attr->u.color;
-  	else
-  	      this->text_foreground_color=COLOR_WHITE;
-! 
-! 	this->text_background_color=COLOR_BLACK;
-  	if( (attr=attr_search(attrs,NULL,attr_columns)))
-  	      this->cols=attr->u.num;
-  	if( (attr=attr_search(attrs,NULL,attr_osd_configuration)))
---- 5901,5908 ----
-  	      this->text_foreground_color=*attr->u.color;
-  	else
-  	      this->text_foreground_color=COLOR_WHITE;
-! 	// SCIP
-! 	this->text_background_color=COLOR_WHITE;
-  	if( (attr=attr_search(attrs,NULL,attr_columns)))
-  	      this->cols=attr->u.num;
-  	if( (attr=attr_search(attrs,NULL,attr_osd_configuration)))
+diff --git a/navit/navit/gui/internal/gui_internal.c b/navit/navit/gui/internal/gui_internal.c
+index 4dadb3f..488b387 100644
+--- a/navit/navit/gui/internal/gui_internal.c
++++ b/navit/navit/gui/internal/gui_internal.c
+@@ -5984,7 +5984,7 @@ static struct gui_priv * gui_internal_new(struct navit *nav, struct gui_methods
+ 	      this->text_foreground_color=*attr->u.color;
+ 	else
+ 	      this->text_foreground_color=COLOR_WHITE;
+-	this->text_background_color=COLOR_BLACK;
++	this->text_background_color=COLOR_WHITE;
+ 	if( (attr=attr_search(attrs,NULL,attr_columns)))
+ 	      this->cols=attr->u.num;
+ 	if( (attr=attr_search(attrs,NULL,attr_osd_configuration)))
