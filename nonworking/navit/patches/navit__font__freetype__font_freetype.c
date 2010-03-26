7471d11e177599c85f69ffbd93963a1d54401845
Malte Schröder <maltesch@gmx.de>
2010-03-26 19:12:03 +0100

    add palm's default font to search list

---
+++ nonworking/navit/patches/navit__font__freetype__font_freetype.c
@@ -0,0 +1,12 @@
+diff --git a/navit/navit/font/freetype/font_freetype.c b/navit/navit/font/freetype/font_freetype.c
+index 4b277a7..ccff8c4 100644
+--- a/navit/navit/font/freetype/font_freetype.c
++++ b/navit/navit/font/freetype/font_freetype.c
+@@ -254,6 +254,7 @@ font_freetype_text_new(char *text, struct font_freetype_font *font, int dx,
+  */
+ static char *fontfamilies[] = {
+ 	"Liberation Sans",
++	"Prelude",
+ 	"Arial",
+ 	"NcrBI4nh",
+ 	"luximbi",
