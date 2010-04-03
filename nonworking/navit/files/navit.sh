6a42eb39fd2d3cdd1fafd0e53762ed598106b850
Malte Schröder <maltesch@gmx.de>
2010-04-03 14:24:02 +0200

    add variables that control espeak output, do some cleaning up

--- nonworking/navit/files/navit.sh
+++ nonworking/navit/files/navit.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-#unset LC_ALL
-export LANG=de_DE.UTF-8
-#export LC_MESSAGES=de_DE.UTF-8
-
+export APP_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.navit
 export NAVIT_USER_DATADIR=/media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0
-#export NAVIT_LOGFILE=$NAVIT_USER_DATADIR/navit.log
-export LD_LIBRARY_PATH=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.navit/lib
+export LD_LIBRARY_PATH=$APP_DIR/lib
 
-test -d $NAVIT_USER_DATADIR || mkdir -p $NAVIT_USER_DATADIR
-test -e $NAVIT_USER_DATADIR/navit.xml || cp dist_files/navit*.xml $NAVIT_USER_DATADIR/
+unset LC_ALL
+export LANG=C
+export ESPEAK_VOICE=en
+export ESPEAK_SINK=pnavigation
+
+#export NAVIT_LOGFILE=$NAVIT_USER_DATADIR/navit.log
 #date > $NAVIT_LOGFILE
 
-pgrep "^navit$" || exec /media/cryptofs/apps/usr/palm/applications/org.webosinternals.navit/bin/navit -c $NAVIT_USER_DATADIR/navit.xml
+test -d $NAVIT_USER_DATADIR || mkdir -p $NAVIT_USER_DATADIR
+test -e $NAVIT_USER_DATADIR/navit.xml || cp $APP_DIR/dist_files/navit*.xml $NAVIT_USER_DATADIR/                                                                 
+pgrep "^navit$" || exec $APP_DIR/bin/navit -c $NAVIT_USER_DATADIR/navit.xml
 
