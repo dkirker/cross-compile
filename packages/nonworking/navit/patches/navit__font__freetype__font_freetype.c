c73ac15b89ff31cc7b0c888068e6212a26e83a41
Malte Schröder <maltesch@gmx.de>
2010-05-30 17:18:49 +0200

    navit: sync with upstream r3323

--- packages/nonworking/navit/patches/navit__font__freetype__font_freetype.c
+++ packages/nonworking/navit/patches/navit__font__freetype__font_freetype.c
@@ -1,5 +1,5 @@
 diff --git a/navit/navit/font/freetype/font_freetype.c b/navit/navit/font/freetype/font_freetype.c
-index 4b277a7..c8f77a8 100644
+index 6add9ad..8dc5815 100644
 --- a/navit/navit/font/freetype/font_freetype.c
 +++ b/navit/navit/font/freetype/font_freetype.c
 @@ -254,6 +254,9 @@ font_freetype_text_new(char *text, struct font_freetype_font *font, int dx,
