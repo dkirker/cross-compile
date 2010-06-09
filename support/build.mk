9961a68697d3ef623ab0f1dbbd618b95e4831f75
Will Dietz <w@wdtz.org>
2010-06-09 10:36:25 -0500

    fix package list, make clobber use same list, build works except setup still needs to be run serially. investigating

--- support/build.mk
+++ support/build.mk
@@ -5,25 +5,17 @@
 #Generate a rule in the form
 #build_dev-sdl/libsdl: build_common/libnapp build_common/piranha (etc)
 #and stash it in the .depends.inc for a given directory
+#also generates clobber rule
 packages/%/.depends.inc: packages/%/Makefile packages/%/.depends
-	echo build_$*:: $(addprefix build_,$(shell cat packages/$*/.depends)) > $@
+	echo "build_$*: $(addprefix build_,$(shell cat packages/$*/.depends))" > $@
+	echo '	$$(MAKE) -C packages/$* stage ARCH=$$(ARCH)' >> $@
+	echo >> $@
+	echo "clobber_$*: " > $@
+	echo '	$$(MAKE) -C packages/$* clobber ARCH=$$(ARCH)' >> $@
 
 #General and simple rule to update the .depends file for a given package
 %/.depends: %/Makefile
-	make -C $* .depends
-
-#This rule expects to be called on things like
-#build_dev-sdl/libsdl
-#Note that the dependencies are dealt with via including those
-#.depends.inc files, here we have the simple task of building it
-build_%::
-	make -C packages/$* ARCH=$(ARCH)
-
-#Fix a small thing I'm too lazy to fix now; surely some playing with
-#the various shell invocations will make this go away
-build_./%::  build_%
-	echo "Built $*!"
-
+	$(MAKE) -C $* .depends
 
 
 #=============
@@ -34,7 +26,7 @@ build_./%::  build_%
 
 #This generates a list of every package category we want
 #(excluding nonworking)
-package_cats = $(shell cd packages; find . -mindepth 1 -maxdepth 1 -type d -not -name nonworking -print)
+package_cats = $(shell cd packages; ls |grep -v "^nonworking$$")
 
 #This is a list of every package we want, builds on the above
 package_dirs := $(shell cd packages; find $(package_cats) -mindepth 1 -maxdepth 1 -type d -print)
@@ -48,9 +40,23 @@ dep_files = $(addsuffix /.depends.inc,$(build_dirs))
 #Target for every single package
 all_targets = $(addprefix build_,$(package_dirs))
 
+#all the clobber targets
+clobber_targets = $(addprefix clobber_,$(package_dirs))
+
 #And, after all that fun has been had, finally we include the depeneds rules
 #we created and now we just have to invoke them as required
+ifneq ("$(INC_DEPS)","")
 include $(dep_files)
+endif
+#Don't delete them, make!
+.PRECIOUS: $(dep_files)
 
 #Rule to build every package using the dependencies we've created
-build_all: $(all_targets)
+buildall: $(all_targets)
+clobberall: $(clobber_targets)
+
+#This rule expects to be called on things like
+#build_dev-sdl/libsdl
+#Note that the dependencies are dealt with via including those
+#.depends.inc files, here we have the simple task of building it
+build_%:
