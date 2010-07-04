8cfed28e5742495c8403ac6a236d17301ba7c09b
Malte Schröder <maltesch@gmx.de>
2010-07-04 18:08:33 +0200

    navit: do some adjustments to get language and default-paths from the PDL

--- packages/apps/navit/files/navit.sh
+++ packages/apps/navit/files/navit.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-APP_DIR=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.navit
-export NAVIT_USER_DATADIR=/media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0
+eval $(./pdl-helper)
+
+APP_DIR=$(dirname $0)
+export NAVIT_USER_DATADIR=$PDL_DataFilePath
 export LD_LIBRARY_PATH=$APP_DIR/lib
 
-locale=`sqlite3 /var/luna/preferences/systemprefs.db 'select value from Preferences where key="locale"'`
-country=`echo "${locale}"|sed 's/,//g;s/ //g;s/^.*countryCode...\([a-z][a-z]\).*$/\1/'|tr '[a-z]' '[A-Z]'`
-lng=`echo "${locale}"|sed 's/,//g;s/ //g;s/^.*languageCode...\([a-z][a-z]\).*$/\1/'`
-
 unset LC_ALL
-export LANG=${lng}_${country}.UTF-8
-export ESPEAK_VOICE=${lng}
+export LANG=${PDL_Language}.UTF-8
+export ESPEAK_VOICE=${LANG%%_*}
 export ESPEAK_SINK=pnavigation
 
 export NAVIT_LOGFILE=$NAVIT_USER_DATADIR/navit.log
@@ -24,6 +22,16 @@ echo "------------------------- Start Navit ----------------------------------" 
 date >> $NAVIT_LOGFILE
 
 test -d $NAVIT_USER_DATADIR || mkdir -p $NAVIT_USER_DATADIR
+
+# Migrate data to new directories
+if test -d /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0 
+then
+	mv /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0/* $NAVIT_USER_DATADIR
+	mv /media/internal/MapsNavit $NAVIT_USER_DATADIR/maps
+	sed -i -re "s/\/media\/internal\/MapsNavit/\$NAVIT_USER_DATADIR\/maps/"  $NAVIT_USER_DATADIR/navit.xml
+	rmdir /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0
+fi
+
 test -e $NAVIT_USER_DATADIR/navit.xml || cp $APP_DIR/dist_files/navit*.xml $NAVIT_USER_DATADIR/                                                                 
 pgrep "^navit$" || exec $APP_DIR/bin/navit -c $NAVIT_USER_DATADIR/navit.xml
 
