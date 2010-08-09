fb8b2eab9c8592ce204c4a33ae7c366c50483224
Malte Schröder <maltesch@gmx.de>
2010-08-09 16:50:41 +0200

    navit: sync navit.sh with productive version; make sure, maps-directory exists

--- packages/apps/navit/files/navit.sh
+++ packages/apps/navit/files/navit.sh
@@ -9,14 +9,18 @@ fi
 eval $($APP_DIR/pdl-helper)
 
 export NAVIT_USER_DATADIR=$PDL_DataFilePath
-export LD_LIBRARY_PATH=$APP_DIR/lib:/media/internal/.local/lib
+export LD_LIBRARY_PATH=$APP_DIR/lib:/media/internal/.widk/usr/lib
 
 unset LC_ALL
 export LANG=${PDL_Language}.UTF-8
 export SPEECHD_SOCKET=/tmp/speechd-sock
 
 export NAVIT_LOGFILE=$NAVIT_USER_DATADIR/navit.log
+
+test -d $NAVIT_USER_DATADIR || mkdir -p $NAVIT_USER_DATADIR/maps
+
 #delete log if it is more the 300kb
+touch $NAVIT_LOGFILE
 if [ `ls -l $NAVIT_LOGFILE | awk '{print $5}'` -gt 300000 ];then
 	rm -f ${NAVIT_LOGFILE}.gz
 	gzip -9 $NAVIT_LOGFILE
@@ -25,7 +29,15 @@ fi
 echo "------------------------- Start Navit ----------------------------------" >> $NAVIT_LOGFILE
 date >> $NAVIT_LOGFILE
 
-test -d $NAVIT_USER_DATADIR || mkdir -p $NAVIT_USER_DATADIR
+# Migrate data to new directories
+if test -d /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0 
+then
+	mv /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0/* $NAVIT_USER_DATADIR
+	mv /media/internal/MapsNavit/* $NAVIT_USER_DATADIR/maps
+	rmdir /media/internal/MapsNavit
+	sed -i -re "s/\/media\/internal\/MapsNavit/\$NAVIT_USER_DATADIR\/maps/"  $NAVIT_USER_DATADIR/navit.xml
+	rmdir /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0
+fi
 
 test -e $NAVIT_USER_DATADIR/navit.xml || cp $APP_DIR/dist_files/navit*.xml $NAVIT_USER_DATADIR/                                                                 
 pgrep "^navit$" || exec $APP_DIR/bin/navit -c $NAVIT_USER_DATADIR/navit.xml
