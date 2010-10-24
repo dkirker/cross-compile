e4fd419f6094153b0558850bdbb628a5a2dcb48b
Malte Schröder <maltesch@gmx.de>
2010-10-24 20:44:25 +0200

    navit: modify topbar

--- packages/apps/navit/patches/navit__gui__internal__gui_internal.c
+++ packages/apps/navit/patches/navit__gui__internal__gui_internal.c
@@ -1,8 +1,32 @@
 diff --git a/navit/navit/gui/internal/gui_internal.c b/navit/navit/gui/internal/gui_internal.c
-index 78d6886..61a42f6 100644
+index 78d6886..cbebfbe 100644
 --- a/navit/navit/gui/internal/gui_internal.c
 +++ b/navit/navit/gui/internal/gui_internal.c
-@@ -6487,7 +6487,9 @@ static struct gui_priv * gui_internal_new(struct navit *nav, struct gui_methods
+@@ -1575,6 +1575,7 @@ gui_internal_top_bar(struct gui_priv *this)
+ 		if (g_list_previous(l) || !g_list_next(l)) {
+ 			wc=l->data;
+ 			wcn=gui_internal_label_new(this, wc->text);
++			wcn->flags |= flags_fill | gravity_ycenter;
+ 			wcn->foreground=foreground;
+ 			if (g_list_next(l))
+ 				use_sep=1;
+@@ -1588,6 +1589,7 @@ gui_internal_top_bar(struct gui_priv *this)
+ 			}
+ 			if (use_sep) {
+ 				struct widget *wct=gui_internal_label_new(this, "»");
++				wct->flags |= flags_fill | gravity_ycenter;
+ 				wct->foreground=foreground;
+ 				res=g_list_prepend(res, wct);
+ 				width_used+=sep_len+w->spx;
+@@ -1616,6 +1618,7 @@ gui_internal_top_bar(struct gui_priv *this)
+ 			wc=l->data;
+ 		}
+ 		wcn=gui_internal_label_new(this, ".. »");
++		wcn->flags |= flags_fill | gravity_ycenter;
+ 		wcn->foreground=foreground;
+ 		wcn->func=gui_internal_cmd_return;
+ 		wcn->data=wc;
+@@ -6487,7 +6490,9 @@ static struct gui_priv * gui_internal_new(struct navit *nav, struct gui_methods
  	      this->text_foreground_color=*attr->u.color;
  	else
  	      this->text_foreground_color=COLOR_WHITE;
