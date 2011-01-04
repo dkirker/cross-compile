e9479a67f897d8450d2e98ddc19c3c32974078fb
Rod Whitby <rod@whitby.id.au>
2011-01-04 19:17:10 +1030

    Fixed problem exporting ARCH into the environment

--- support/staging.mk
+++ support/staging.mk
@@ -2,6 +2,8 @@ ifeq ("${ARCH}", "")
 ARCH = armv7
 endif
 
+unexport ARCH
+
 STAGING_DIR := $(shell cd ../../../staging/${ARCH}; pwd)
 
 MAPPING_FILE := $(shell cd ../../../staging; pwd)/mapping-${ARCH}
