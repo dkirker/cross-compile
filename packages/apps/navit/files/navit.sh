83dc627515581368768bef1b35a8d30bc47e9f56
Malte Schröder <maltesch@gmx.de>
2011-02-12 18:48:48 +0100

    navit: Remove old migration stuff, shouldn't be needed anymore

--- packages/apps/navit/files/navit.sh
+++ packages/apps/navit/files/navit.sh
@@ -35,16 +35,6 @@ fi
 echo "------------------------- Start Navit ----------------------------------" >> $NAVIT_LOGFILE
 date >> $NAVIT_LOGFILE
 
-# Migrate data to new directories
-if test -d /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0 
-then
-	mv /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0/* $NAVIT_USER_DATADIR
-	mv /media/internal/MapsNavit/* $NAVIT_USER_DATADIR/maps
-	rmdir /media/internal/MapsNavit
-	sed -i -re "s/\/media\/internal\/MapsNavit/\$NAVIT_USER_DATADIR\/maps/"  $NAVIT_USER_DATADIR/navit.xml
-	rmdir /media/internal/.app-storage/file_.media.cryptofs.apps.usr.palm.applications.org.webosinternals.navit_0
-fi
-
 test -e $NAVIT_USER_DATADIR/navit.xml || cp -R $APP_DIR/dist_files/* $NAVIT_USER_DATADIR/                                                                 
 pgrep "^navit$" || exec $APP_DIR/bin/navit -c $NAVIT_USER_DATADIR/navit.xml 2>&1 | tee -a $NAVIT_LOGFILE
 
